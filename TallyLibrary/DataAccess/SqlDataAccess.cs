4ab70bba4f9d7434f986dbbba6673bccd268962d
Aadi Poddar <aadipoddarmail@gmail.com>
2024-07-15 13:58:07 +0530

    Get Server Name

--- TallyLibrary/DataAccess/SqlDataAccess.cs
+++ TallyLibrary/DataAccess/SqlDataAccess.cs
@@ -1,5 +1,6 @@
 ﻿using System.Data;
 using System.Data.SqlClient;
+using System.Runtime.InteropServices;
 
 using Dapper;
 
@@ -7,8 +8,22 @@ namespace TallyLibrary.DataAccess;
 
 static class SqlDataAccess
 {
-	public static string GetConnectionstring(string databaseName) =>
-		$"Server=AADIKIITLAPI\\SQLEXPRESS;Integrated security=SSPI;database={databaseName}";
+	public static string GetConnectionstring(string databaseName)
+	{
+		List<string> cmbServerName = new();
+
+		if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+		{
+			Microsoft.Win32.RegistryView registryView = Environment.Is64BitOperatingSystem ? Microsoft.Win32.RegistryView.Registry64 : Microsoft.Win32.RegistryView.Registry32;
+			using Microsoft.Win32.RegistryKey hklm = Microsoft.Win32.RegistryKey.OpenBaseKey(Microsoft.Win32.RegistryHive.LocalMachine, registryView);
+			Microsoft.Win32.RegistryKey instanceKey = hklm.OpenSubKey(@"SOFTWARE\Microsoft\Microsoft SQL Server\Instance Names\SQL", false);
+			if (instanceKey != null)
+				foreach (var instanceName in instanceKey.GetValueNames())
+					cmbServerName.Add(Environment.MachineName + "\\" + instanceName);
+		}
+
+		return $"Server={cmbServerName.FirstOrDefault()};Integrated security=SSPI;database={databaseName}";
+	}
 
 	public static async Task RunSQL(string sql, string databaseName)
 	{
@@ -16,7 +31,7 @@ static class SqlDataAccess
 
 		await connection.ExecuteAsync(sql);
 	}
-
+	
 	public static async Task<IEnumerable<T>> LoadDataSQL<T>(string sql, string databaseName)
 	{
 		using IDbConnection connection = new SqlConnection(GetConnectionstring(databaseName));
